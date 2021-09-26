3267eea4e55f0ed5ae6f276c4f30521bf45bec17
RockMiin <52374789+RockMiin@users.noreply.github.com>
2021-09-26 15:57:49 +0900

    Add files via upload

---
+++ DP/11053_가장긴증가하는부분수열_최석민.py
@@ -0,0 +1,23 @@
+
+n= int(input())
+num= list(map(int, input().split()))
+dp= [1]* (n)
+
+for i in range(n):
+    for j in range(i):
+        if num[i]> num[j]:
+            dp[i]= max(dp[i], dp[j]+1)
+    # print(dp)
+print(max(dp))
+
+# 틀린 version
+n= int(input())
+num= list(map(int, input().split()))
+dp= [1]* (n+1)
+
+for i in range(1, n+1):
+    for j in range(i):
+        if num[i-1] > num[j-1]:
+            dp[i]= max(dp[i], dp[j]+1)
+
+print(max(dp))
