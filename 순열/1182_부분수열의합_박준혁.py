4b78b74755800a9cabc79db7f4710121b68bbfd5
ParkJunHyuk <akznsk1@gmail.com>
2021-11-08 23:49:44 +0900

    1182 부분수열의합 제출

---
+++ 순열/1182_부분수열의합_박준혁.py
@@ -0,0 +1,13 @@
+from itertools import *
+
+N,S = map(int,input().split())
+num_list = list(map(int,input().split()))
+
+answer = 0
+
+for i in range(1,N):
+    for c in combinations(num_list,i):
+        if S == sum(c):
+            answer += 1
+
+print(answer)
