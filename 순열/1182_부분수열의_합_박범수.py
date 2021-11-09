9dc62500751145c10f0f7ab5ae4a0e999f9827ce
hanlyang0522 <500nyanpark@gmail.com>
2021-11-09 18:43:38 +0900

    수열, 월코챌3 풀이 업로드

---
+++ 순열/1182_부분수열의_합_박범수.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import combinations
+
+f = sys.stdin.readline
+
+N, S = map(int, f().split())
+
+li = list(map(int, f().split()))
+
+print(li)
+cnt = 0
+
+for i in range(1, N + 1):
+    for c in combinations(li, i):
+        if sum(c) == S:
+            cnt += 1
+
+print(cnt)
